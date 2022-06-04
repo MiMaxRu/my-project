5f7e22edb50b008aafb1ec04e3a28132f5cb8b66
MiMaxRu <106865474+MiMaxRu@users.noreply.github.com>
2022-06-04 19:36:31 +0300

    my-project
    my-project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+
+
+function App() {
+  return (
+    <div className="App-wrapper">
+
+<Header />
+<Navbar />
+<Profile />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = ()=>{
+    return <div className='content'>
+    <div> 
+    <img src ='https://get.wallhere.com/photo/landscape-sea-reflection-bridge-suspension-bridge-1680x1050-px-nonbuilding-structure-cable-stayed-bridge-truss-bridge-706446.jpg'></img>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>My post</div>
+    <div>New post</div>
+    
+    <div>
+      <div>post1</div>
+      <div>post2</div>
+    </div>
+    </div>
+}
+export default Profile;
